6b812ec8370f8d297f2f34ae8f0ef9920dd9d770
Huong Trinh <trtrhuong@gmail.com>
2019-04-19 22:17:14 +0700

    create config.py, set secret_key and  database_url, connect database in app.py

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
-from flask import Flask 
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
+
+#set config here
+app.config.from_object(Config)
+
+#connect to database
+db = SQLAlchemy(app)
+
+
+#routes here
+@app.route('/')
+@app.route('/home')
+def home():
+    return 'hello'
