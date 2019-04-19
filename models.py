632a603f59d5e845cf9a73d817ea06a3632bdc63
Huong Trinh <trtrhuong@gmail.com>
2019-04-20 04:56:34 +0700

    modify gitignore

--- models.py
+++ models.py
@@ -1,7 +1,9 @@
 from app import db
 from datetime import datetime
- 
-class User(db.Model):
+from flask_login import UserMixin
+
+# DATABASES HERE 
+class User(UserMixin, db.Model):
     id= db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), index=True, unique=True, nullable=False)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -24,14 +26,10 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, default = datetime.utcnow, server_default= db.func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
-    topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'))
-    # like_number = db.Column(db.Integer, default=0)
-    # comment_number = db.Column(db.Integer, default=0)
+    topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'), nullable=True )
     comments = db.relationship('Comment', backref='post', lazy='dynamic')
     likes = db.relationship('Like', backref='post', lazy='dynamic')
     bookmarks = db.relationship('Bookmark', backref='post', lazy='dynamic')
-
-
 
 class Topic(db.Model):
     id= db.Column(db.Integer, primary_key = True)
@@ -40,7 +38,7 @@ class Topic(db.Model):
     created_at = db.Column(db.DateTime, default = datetime.utcnow, server_default= db.func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     posts = db.relationship('Post', backref='topic', lazy='dynamic')
-    topic_members = db.relationship('Topic_member', backref='topic', lazy='dynamic')
+    topic_members = db.relationship('Topic_member', backref='topic')
 
 
 class Comment(db.Model):
@@ -49,7 +47,7 @@ class Comment(db.Model):
     created_at = db.Column(db.DateTime, default = datetime.utcnow, server_default= db.func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
-    reaction_id = db.Column(db.Integer, db.ForeignKey('reaction.id'))
+    # reaction_id = db.Column(db.Integer, db.ForeignKey('reaction.id'))
 
 class Like(db.Model):
     id= db.Column(db.Integer, primary_key = True)
@@ -59,7 +57,7 @@ class Like(db.Model):
 
 class Topic_member(db.Column):
     id= db.Column(db.Integer, primary_key = True)
-    topic_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 class Category(db.Model):
@@ -80,14 +78,56 @@ class Bookmark(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 
-# class Reaction(db.Model):
-#     id= db.Column(db.Integer, primary_key = True)
-#     icon_url = db.Column(db.String(200), nullable=False)
+def create_category(name):
+    c = Category(name=name)
+    db.session.add(c)
+    db.session.commit()
+
+# create_category('Frontend Development')
+# create_category('Backend Development')
+# create_category('Design UI/UX')
+
+################    
+#ROUTES HERE
+# @app.route('/')
+# @app.route('/home')
+# def home():
+#     return 'hello'
+
+# @app.route('/login')
+# def login():
+#     return render_template('login.html')
+
+# @app.route('/create_post', methods=('GET', 'POST'))
+# # @login_required
+# def createPost():
+#     # if not current_user.is_authenticated:
+#     #     flash='please login to create new post'
+#     #     return redirect(url_for('login'))
+#     # else:
+#     if request.method == 'POST':
+#         title = request.form['title']
+#         category_id = request.form['category']
+#         content = request.form['content']
+#         # topic_id = 
+#         if title and content and category_id:
+#             create_post(title, content, int(category_id), topic_id=None)
+#             return redirect(url_for('home'))
+#         else:
+#             flash('invalid input')
+
+#     return render_template('create_post.html')
+
+# def create_post(title, content, category_id, user_id = 1, topic_id=None):
+#     p = Post(title=title, content=content, user_id=user_id, category_id=category_id, topic_id=topic_id)
+#     db.session.add(p)
+#     db.session.commit()
 
 
-    
-
-
+# #always put this at bottom
+# @login_manager.user_loader
+# def load_user(id):
+#     return User.query.get(int(id))
 
 
 
