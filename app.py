62e3fa809b3be25bb3592b6e76f4c78d5111218c
kim111 <KimTrijnh@users.noreply.github.com>
2019-04-20 06:45:04 +0700

    B/db migrate (#3)
    * create basic Classes for databases
    
    * create 9 tables (Classes) in models.py
    
    * create create_post.html template
    
    * modify gitignore
    
    * db
    
    * add migrate
    
    * del rows in Category
    
    * db
    
    * del redudent rows in category
    
    * modify gitignore
    
    * add 2 function password_hash to class User
    
    * move db from models to app.py
    
    * complete setting basic app.py

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 
 app = Flask(__name__)
 
-#set config here
+#set config here, already set SECRET_KEY, DATABASE_URL
 app.config.from_object(Config)
 
-#connect to database
+## DATABSE CONNECT below
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+from models import User, Post, Topic, Comment, Event, Bookmark, Like, Topic_member, Category
+
+## DATABASE CONNECT above
 
 
-#routes here
+###  ROUTES HERE
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'hello'
+    u = User.query.filter_by(id=1).first()
+    return u.username
+
