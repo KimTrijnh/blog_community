591bc9ec20eadb64af5e97491f64f669c6b13d05
kim111 <KimTrijnh@users.noreply.github.com>
2019-04-20 19:58:12 +0700

    H/db updated (#4)
    * create config.py, set secret_key and  database_url, connect database in app.py
    
    * create html files in templates folder
    
    * create basic Classes for databases
    
    * create 9 tables (Classes) in models.py
    
    * create create_post.html template
    
    * modify gitignore
    
    * db
    
    * add migrate
    
    * del rows in Category
    
    * db
    
    * del redudent rows in category
    
    * modify gitignore
    
    * add 2 function password_hash to class User
    
    * move db from models to app.py
    
    * complete setting basic app.py
    
    * clear gitignore
    
    * create post.html def post
    
    * modify bookmarks and topic_member into association tables
    
    * change db
    
    * pc
    
    * clear app.py

--- app.py
+++ app.py
@@ -1,3 +1,73 @@
-from flask import Flask 
+from flask import Flask, render_template, redirect, url_for, flash, request
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from datetime import datetime
+from flask_login import UserMixin, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager
+
+
 
 app = Flask(__name__)
+
+#set config here, already set SECRET_KEY, DATABASE_URL
+app.config.from_object(Config)
+
+## DATABSE CONNECT below
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+from models import User, Post, Topic, Comment, Event, Like, subs, bookmarks, Category
+
+## DATABASE CONNECT above
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+###  ROUTES HERE
+@app.route('/')
+@app.route('/home')
+def home():
+    return 'huong'
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+
+# @app.route('/create_post', methods=('GET','POST'))
+# # @login_required
+# def create_post():
+#     current_user = User.query.filter_by(id=1).first()
+#     if not current_user.is_authenticated:
+#         flash('please login to create post')
+#         return redirect(url_for('login'))
+#     else:
+#         if request.method == 'POST':
+#             title = request.form['title']
+#             content = request.form['content']
+#             user_id = 1
+#             category = request.form['category']
+#             topic_id = None
+#             if title and content and category:
+#                 p = Post(title = title, content = content, user_id = user_id, category_id = int(category), topic_id=topic_id)
+#                 db.session.add(p)
+#                 db.session.commit()
+#                 flash('your post is successfull pulished')
+#                 return redirect(url_for('post', post_id= p.id))
+#             else:
+#                 flash('please check your title/content/catagory')
+#     return render_template('create_post.html')
+
+
+# @app.route('/post/<int:post_id>')
+# def post(post_id):
+#     p = Post.query.filter_by(id=post_id).first()
+#     author = User.query.filter_by(id=p.user_id).first()
+#     category = Category.query.filter_by(id=p.category_id).first()
+#     return render_template('post.html', post = p, author = author, category = category)
---
+++ models.py
@@ -0,0 +1,127 @@
+from app import db
+from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# DATABASES HERE
+
+
+subs = db.Table('subs', 
+db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+db.Column('topic_id', db.Integer, db.ForeignKey('topic.id')))
+
+bookmarks = db.Table('bookmarks', 
+db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+db.Column('user.id', db.Integer, db.ForeignKey('user.id')))
+
+
+class User(UserMixin, db.Model, object):
+    id= db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(120), nullable=False)
+    posts = db.relationship('Post', backref='owner', lazy='dynamic')
+    likes = db.relationship('Like', backref='owner', lazy='dynamic')
+    posts= db.relationship('Post', backref='owner', lazy='dynamic')
+    topics = db.relationship('Topic', backref='owner', lazy='dynamic')
+    comments = db.relationship('Comment', backref='owner', lazy='dynamic')
+    likes = db.relationship('Like', backref='owner', lazy='dynamic')
+    events = db.relationship('Event', backref='owner', lazy='dynamic')
+    subscriptions = db.relationship('Topic', secondary='subs', backref=db.backref('subscribers', lazy='dynamic'))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+
+class Topic(db.Model):
+    id= db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), index=True, nullable=False)
+    description = db.Column(db.String(260), nullable=False)
+    created_at = db.Column(db.DateTime, default = datetime.utcnow, server_default= db.func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    posts = db.relationship('Post', backref='topic', lazy='dynamic')
+
+
+
+class Post(db.Model):
+    id= db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), index=True, nullable=False)
+    content = db.Column(db.Text, index=True, nullable=False)
+    created_at = db.Column(db.DateTime, default = datetime.utcnow, server_default= db.func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'), nullable=True )
+    comments = db.relationship('Comment', backref='post', lazy='dynamic')
+    likes = db.relationship('Like', backref='post', lazy='dynamic')
+    bookmarks = db.relationship('User', secondary='bookmarks', backref=db.backref('posts', lazy='dynamic'))
+
+
+
+
+class Comment(db.Model):
+    id= db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(400), index=True, nullable=False)
+    created_at = db.Column(db.DateTime, default = datetime.utcnow, server_default= db.func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    # reaction_id = db.Column(db.Integer, db.ForeignKey('reaction.id'))
+
+class Like(db.Model):
+    id= db.Column(db.Integer, primary_key = True)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+
+
+class Category(db.Model):
+    id= db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), index=True, nullable=False)
+    posts = db.relationship('Post', backref='category', lazy='dynamic')
+
+class Event(db.Model):
+    id= db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), index=True, nullable=False)
+    description = db.Column(db.String(260), nullable=False)
+    created_at = db.Column(db.DateTime, default = datetime.utcnow, server_default= db.func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+
+
+
+# class Bookmark(db.Model):
+#     id= db.Column(db.Integer, primary_key = True)
+#     post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+# class Topic_member(db.Model):
+#     id= db.Column(db.Integer, primary_key = True)
+#     topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'), nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+## FUNCTIONS TO ADJUST DATABASE TABLE'S
+# db.create_all()
+def create_category(name):
+    c = Category(name=name)
+    db.session.add(c)
+    db.session.commit()
+
+# create_category('Frontend Development')
+# create_category('Backend Development')
+# create_category('Design UI/UX')
+
+def del_row_category(id):
+    c = Category.query.filter_by(id=id).first()
+    db.session.delete(c)
+    db.session.commit()
+
+
+
+
+
+
+
+
